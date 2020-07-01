153ff67e4b7ce366cda75b42e016e21ce62cb9ac
Steve <sfhazenberg@gmail.com>
2020-07-01 19:57:10 +0200

    Add files via upload

---
+++ C#/BallAdventure/Assets/BridgeMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeMover : MonoBehaviour {
+
+    public Transform destination;
+    public PlayerController PlayerController;
+    public float bridgeSpeed;
+    //private int coinCounter;
+    public bool coinsCollected;
+
+	// Use this for initialization
+	void Start () {
+        coinsCollected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (coinsCollected) {
+            float step = bridgeSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, destination.position, step);
+        }
+	}
+
+    public void MoveBridge() {
+        coinsCollected = true;
+    }
+}
---
+++ C#/BallAdventure/Assets/Floor3Trigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor3Trigger : MonoBehaviour {
+
+    public ElevWallE ElevWallE;
+    public Floor3Door Floor3Door;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            print("Floor3Trigger active");
+            Floor3Door.LowerDoor();
+            ElevWallE.LowerDoor();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Floor3Door.RaiseDoor();
+            ElevWallE.RaiseDoor();
+        }
+    }
+}
