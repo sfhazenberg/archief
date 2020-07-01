00ed419df403275f0de314107269c15de2d5a8c6
Steve <sfhazenberg@gmail.com>
2020-07-01 19:36:03 +0200

    Add files via upload

---
+++ Java/JavaFundamentals/Part_01/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Is this the Krusty Krab?");
+    }
+}
+
+/*
+ * String is een class, niet een primitive
+ * boolean isBirthdayToday: het "is"-gedeelte geeft aan dat de variabele een boolean. 'Best practice', pas het toe.
+ * als een constant wordt gebruikt: zet keyword 'final' ervoor. Zet de constante helemaal in hoofdletters en gebruik underscores in de naam v/d variabele.
+ * Widening Casting (van klein naar groot) (deze vorm van casting doet Java automatisch)
+ *  byte -> short -> char -> int -> long -> float -> double
+ * Narrowing Casting (van groot naar klein) (moet handmatig)
+ *  double -> float -> long -> int -> char -> short -> byte
+ */
